3c6b516ccc8fcb05f9e41df6d48404a41f23917b
cmmbranco <carlos.m.branco@tecnico.ulisboa.pt>
2018-10-22 23:55:58 +0100

    fix name in forest chart and untrack .metadata

--- scania_part/scania_ec/forest_kfold.py
+++ scania_part/scania_ec/forest_kfold.py
@@ -1,98 +1,105 @@
-import logging
-from threading import Thread
 from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-from sklearn.metrics.classification import accuracy_score
-#from sklearn.model_selection import StratifiedKFold
 import numpy as np
+from scipy import interp
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import label_binarize
 
-results = {}
+from sklearn.neighbors.classification import KNeighborsClassifier
 
-def threadedRForest(k,n_fold,output):
 
-    try:
-        ###Main LOOP
 
-        data = pd.read_csv("../scania_dataset/aps_failure_training_set.csv")
-        data = data.replace({'na': '0'}, regex=True)
+n_fold = 10
+k = 53
+
+# #############################################################################
+# Data IO and generation
+
+# Import some data to play with
+
+data = pd.read_csv("../scania_dataset/aps_failure_training_set.csv")
+data = data.replace({'na': '-1'}, regex=True)
         
-        data2 = pd.read_csv("../scania_dataset/aps_failure_test_set.csv")
-        data2 = data2.replace({'na': '0'}, regex=True)
+X=data.iloc[:,1:]
+X = np.asarray(X)
         
-        X = data.iloc[:,1:]
-        X = np.asarray(X)
+Y=data['class']
+labels = pd.unique(Y)
+Y = np.asarray(Y)
         
-        X1 = data2.iloc[:,1:]
-        X1 = np.asarray(X1)
-        
-        Y = data['class']
-        Yvals = pd.unique(Y)
-        
-        Y1 = data2['class']
-        
-        
-        #kf = StratifiedKFold(n_splits=n_fold, random_state=None, shuffle=False)
-        #kf.get_n_splits(data)
+kf = StratifiedKFold(n_splits=n_fold, random_state=None, shuffle=False)
+kf.get_n_splits(data)
         
         
             
-        scores = []
-        x_train = X
-        y_train = Y
-        x_test = X1
-        y_test = Y1
-
-        clf = RandomForestClassifier(n_estimators=k)
-
-        model = clf.fit(x_train, y_train)
-
-        results = clf.predict(x_test)
-            
-        score = accuracy_score(y_test,results)
-        print("model score acquired")    
-        scores.append(score)
-                
-        median = np.median(scores)
-        #print(f"median for kneighbors = {k} and n-folds = {nsplits} was {median}")
-            
-        standdev= np.std(scores)
-        #print(f"standard deviation for kneighbors = {k} and n-fiks was {standdev}")
-        
-        output[k] = (n_fold,median,standdev)
-        
-    except Exception as e: 
-        print(e)
-        
-    return True
+scores = []
+x_train = []
+y_train = []
+x_test = []
+y_test = []
 
 
 
 
+# #############################################################################
+# Classification and ROC analysis
 
-#create a list of threads
-threads = []
+# Run classifier with cross-validation and plot ROC curves
+cv = StratifiedKFold(n_splits=n_fold)
+clf = RandomForestClassifier()
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
+
+i = 0
+
+for train_index, test_index in kf.split(X,Y):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    x_train, x_test = X[train_index], X[test_index]
+    y_train, y_test = Y[train_index], Y[test_index]
+    
+    # Binarize the output
+    y_test_bin = label_binarize(y_test, labels)
+    #n_classes_nb = y_test_bin_nb.shape[1]
 
 
-#,33,65,399,401)
-n_folds = 3
-n_estim = (5,10,20,30,50,100)
-result={}
-for k in n_estim:
+    probas_ = clf.fit(x_train, y_train).predict_proba(x_test)
+    # Compute ROC curve and area the curve
+    fpr, tpr, thresholds = roc_curve(y_test_bin, probas_[:, 1])
+    tprs.append(interp(mean_fpr, fpr, tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    aucs.append(roc_auc)
+    plt.plot(fpr, tpr, lw=1, alpha=0.3,
+             label='ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
+
+    i += 1
     
     
-    
-    process = Thread(target=threadedRForest, args=[k,n_folds, result])
-    print(f"Starting CART with {k} forests")
-    process.start()
-    threads.append(process)
-    
-        
+plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+         label='Random', alpha=.8)
 
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='b',
+         label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+         lw=2, alpha=.8)
 
-for process in threads:
-        process.join()
-            
-        
-print(result)
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                 label=r'$\pm$ 1 std. dev.')
 
-
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title(f'ROC Chart for Random Forest with k = {k} and {n_fold} folds')
+plt.legend(loc="lower right")
+plt.show()
