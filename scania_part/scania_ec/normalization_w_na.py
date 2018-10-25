8c29e7663a2dd2f55fe731eac3b446384834a837
joao-neves-silva <44029516+joao-neves-silva@users.noreply.github.com>
2018-10-25 15:03:35 +0100

    changed normalization_w_na to be a function

--- scania_part/scania_ec/normalization_w_na.py
+++ scania_part/scania_ec/normalization_w_na.py
@@ -1,27 +1,34 @@
 import pandas as pd
 import numpy as np
 
+
+def normalize (dataframe):
+    maximums=[]
+    minimums=[]
+    dataframe_normalized=pd.DataFrame((np.zeros(dataframe.shape)))
+
+    #Calculating the max and min for each attribute
+    for col in range(dataframe.shape[1]):
+        col_max=np.amax(dataframe.iloc[:,col])
+        col_min=np.amin(dataframe.iloc[:,col])
+        maximums.append(col_max)
+        minimums.append(col_min)
+    
+
+    for col in range(dataframe.shape[1]): 
+        for row in range (dataframe.shape[0]):
+            print('row: {}   col:{}'.format(row,col))
+            if dataframe.iloc[row,col] == np.nan:
+                dataframe_normalized.iloc[row,col]=np.nan
+                
+            else:
+                dataframe_normalized.iloc[row,col]= (dataframe.iloc[row,col] - minimums[col]) / (maximums[col] - minimums[col])
+
+    return dataframe_normalized
+
+
 data = pd.read_csv("../scania_dataset/aps_failure_training_set.csv", na_values='na')
 
 X=data.iloc[:,1:]
 
-maximums=[]
-minimums=[]
-X_normalized=np.zeros(X.shape)
-
-#Calculating the max and min for each attribute
-for col in range(X.shape[1]):
-    col_max=np.amax(X.iloc[:,col])
-    col_min=np.amin(X.iloc[:,col])
-    maximums.append(col_max)
-    minimums.append(col_min)
-    
-
-for col in range(2): #X.shape[1]
-    for row in range (X.shape[0]):
-        print('row: {}   col:{}'.format(row,col))
-        if X.iloc[row,col] == np.nan:
-            X_normalized[row][col]=np.nan
-            
-        else:
-            X_normalized[row][col]= (X.iloc[row,col] - minimums[col]) / (maximums[col] - minimums[col])
+X_normalized=normalize(X)
