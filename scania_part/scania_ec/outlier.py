79ccbb4c4a9c999a8a44cf835488ada99438888f
Rodrigojflima <drigo.a.lima@gmail.com>
2018-10-27 22:22:39 +0100

    Some almost insignificant changes

--- scania_part/scania_ec/outlier.py
+++ scania_part/scania_ec/outlier.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy.stats
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import scipy.stats
 
 
 
